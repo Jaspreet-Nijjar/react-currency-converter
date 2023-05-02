275c9b8faa915a61e9a381704ecd980147fe3841
Jaspreet <jaspreetkn92@gmail.com>
2023-05-02 10:38:53 +0100

    styling the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { CurrencyRow } from './CurrencyRow';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Currency Converter</h1>
+        <CurrencyRow />
+        <div className="equals">=</div>
+        <CurrencyRow />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/CurrencyRow.js
@@ -0,0 +1,10 @@
+export const CurrencyRow = () => {
+  return (
+    <div>
+      <input type="number" className="input" />
+      <select>
+        <option value="test">Test</option>
+      </select>
+    </div>
+  );
+};
